4eb8628372947ee3abe860976ec7826c58a57a42
Yoshiteru <289678850+Yoshiteru-Akiyama-GeoAI@users.noreply.github.com>
2026-07-28 23:06:01 +0900

    Update transformer

--- models/transformer.py
+++ models/transformer.py
@@ -6,39 +6,7 @@ import math
 # Transformer
 # - Self-Attention
 # - Last / Attention / Masked GAP
-# * PositionalEncoding is negative effect when DOY is used.
 ##############################################################################
-class PositionalEncoding(nn.Module):
-
-    def __init__(
-        self,
-        d_model,
-        max_len=500
-    ):
-        super().__init__()
-
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float32).unsqueeze(1)
-
-        div_term = torch.exp(
-            torch.arange(
-                0,
-                d_model,
-                2,
-                dtype=torch.float32
-            )
-            * (-math.log(10000.0) / d_model)
-        )
-
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0)
-        self.register_buffer("pe", pe)
-
-    def forward(self, x):
-        seq_len = x.size(1)
-        return x + self.pe[:, :seq_len]
-
 
 class CropTransformer(nn.Module):
 
@@ -59,7 +27,6 @@ class CropTransformer(nn.Module):
         super().__init__()
 
         self.input_proj = nn.Linear(n_features, d_model)
-        self.pos_encoder = PositionalEncoding(d_model=d_model)
 
         encoder_layer = nn.TransformerEncoderLayer(
             d_model=d_model,
@@ -86,7 +53,6 @@ class CropTransformer(nn.Module):
 
         # x (batch, seq_len, 9)
         x = self.input_proj(x)
-#        x = self.pos_encoder(x)    # *Negative Effect due to DOY
         batch_size, seq_len, _ = x.shape
 
         mask = (
